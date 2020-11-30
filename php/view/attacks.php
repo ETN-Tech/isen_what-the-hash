4d8b4a46b7765ae3b06ceec7f0836c30b60593fa
ETN-Tech <etienne.schelfhout.pro@gmail.com>
2020-11-30 17:06:41 +0100

    Updated presentation css

--- php/view/attacks.php
+++ php/view/attacks.php
@@ -1,6 +1,14 @@
 <main class="bd-content container">
-    <h1>Attacks</h1>
-    <p>Lots of research about attacks on cryptographic algorithms are conducted. Here are the attacks researchers and hackers focus on, to try to break hash functions. Hash functions are considered ti be broken by an attack if this one requires less tries than a brute force attack.</p>
+    <div class="row">
+        <div class="col-8">
+            <div class="up-content">
+                <h1 class="up-title mb-4 pb-3">Attacks</h1>
+                <p class="up-lead">What are the attacks against cryptographic hash functions ?</p>
+                <p class="up-text text-muted">Many attacks exist to try to break cryptographic hash functions. Some of them can successfully break a hash function, others are currently technically impossible to accomplish.</p>
+                <p class="up-text text-muted">A hash function is considered to be broken by an attack if this one requires less tries than a brute force attack.</p>
+            </div>
+        </div>
+    </div>
 
     <? while ($attack = $attacks->fetch()) { ?>
         <article class="py-1 my-2">
--- php/view/implications.php
+++ php/view/implications.php
@@ -1,8 +1,12 @@
 <main class="bd-content container">
-    <div class="up-content">
-        <h1 class="up-title mb-4 pb-2">Implications</h1>
-        <p>Why is hash function security crucial ?</p>
-        <p>Hash functions are used in a very large scope nowadays. Data security, data integrity, authentication, crypto-money...</p>
+    <div class="row">
+        <div class="col-8">
+            <div class="up-content">
+                <h1 class="up-title mb-4 pb-3">Implications</h1>
+                <p class="up-lead">Why is hash function security crucial ?</p>
+                <p class="up-text text-muted">Hash functions are used in a very large scope nowadays. Data security, data integrity, authentication, crypto-currencies...</p>
+            </div>
+        </div>
     </div>
 
     <? while ($implication = $implications->fetch()) { ?>
