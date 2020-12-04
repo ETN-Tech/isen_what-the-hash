b0fb4e7e637bc34c13381c0df9a79df5cc598768
ETN-Tech <etienne.schelfhout.pro@gmail.com>
2020-12-04 22:31:47 +0100

    Added Blake2 function and hash-functions lab

---
+++ php/lab/hash-functions.php
@@ -0,0 +1,27 @@
+<?php
+
+$supported_algo = array();
+// add other algos natively supported by php
+for ($i = 0; $i <= 13; $i++) {
+    array_push($supported_algo, hash_algos()[$i]);
+}
+array_push($supported_algo, 'blake2');
+
+if (isset($_POST['form-hash-functions'])) {
+    $algo_name = htmlspecialchars($_POST['algo']);
+    $text = htmlspecialchars($_POST['text']);
+
+    // verify if algo is supported
+    if (array_search($algo_name, $supported_algo)) {
+
+        if (array_search($algo_name, hash_algos()) !== false) {
+            $hash = hash($algo_name, $text);
+        } else {
+            switch ($algo_name) {
+                case 'blake2':
+                    $hash = blake2($text);
+                    break;
+            }
+        }
+    }
+}
