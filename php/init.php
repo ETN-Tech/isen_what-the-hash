798b4cffee9a47bc16772c29d20337e6282d450d
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-10-11 12:05:21 +0200

    Added PG support

--- php/init.php
+++ php/init.php
@@ -14,7 +14,7 @@ try {
     $dbopts = parse_url(getenv('DATABASE_URL'));
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 
-    $bdd = new PDO('pgsql:host='. $dbopts["host"] .';port='. $dbopts["port"] .';dbname='. ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"]);
+    $bdd = new PDO('mysql:host='. $dbopts["host"] .';port='. $dbopts["port"] .';dbname='. ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"]);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
