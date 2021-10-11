f0c7a4ba2513f4ded67b1c9ac04648093b34e818
ETN-Tech <43611141+ETN-Tech@users.noreply.github.com>
2021-10-11 14:25:43 +0200

    Updated database connection

--- php/init.php
+++ php/init.php
@@ -8,13 +8,16 @@ date_default_timezone_set('Europe/Paris');
 
 try {
     if (getenv('DATABASE_URL') === false) {
-        putenv("DATABASE_URL=sql://root:root@localhost:3306/isen_caas");
+        putenv("DATABASE_URL=mysql://root:root@localhost:3306/isen_caas");
     }
 
     $dbopts = parse_url(getenv('DATABASE_URL'));
-    $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 
-    $bdd = new PDO('mysql:host='. $dbopts["host"] .';port='. $dbopts["port"] .';dbname='. ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"]);
+    if ($dbopts["scheme"] == "postgres") {
+        $dbopts["scheme"] = "pgsql";
+    }
+
+    $bdd = new PDO($dbopts["scheme"] .':host='. $dbopts["host"] .';port='. $dbopts["port"] .';dbname='. ltrim($dbopts["path"],'/'), $dbopts["user"], $dbopts["pass"]);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
